b34a74155a62433d0fcccdf7d7a44c8140ed3fb4
sohanur28 <gsrahman28@gmail.com>
2026-02-10 09:24:43 +0600

    load json data using fetch

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-console.log("Explore API");
-
-const person = {
-    name: "selim",
-    fruit: "dalim",
-    dish: "halim",
-    freinds: ["alim", "kolim", "lamim"],
-    isRich: false,
-    money: 34000,
+const loadData = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(res => res.json())
+        .then(data => console.log(data));
 };
-console.log(person);
-// JSON -> JS object with Notation
-//JSON.stringify -> 
 
-const personJSON=JSON.stringify(person);
-console.log(personJSON, typeof personJSON);
 
-const parseJSON=JSON.parse(personJSON);
-console.log(parseJSON);
+// console.log("Explore API");
+
+// const person = {
+//     name: "selim",
+//     fruit: "dalim",
+//     dish: "halim",
+//     freinds: ["alim", "kolim", "lamim"],
+//     isRich: false,
+//     money: 34000,
+// };
+// console.log(person);
+// // JSON -> JS object with Notation
+// //JSON.stringify ->
+
+// const personJSON=JSON.stringify(person);
+// console.log(personJSON, typeof personJSON);
+
+// const parseJSON=JSON.parse(personJSON);
+// console.log(parseJSON);
