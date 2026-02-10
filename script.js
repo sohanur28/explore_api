0c9e1f80dd64113c5b8069b30558512833dace4c
sohanur28 <gsrahman28@gmail.com>
2026-02-10 10:26:03 +0600

    forEach data

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const loadData = () => {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
+        // promise of response
         .then(res => res.json())
+        // promise of json
         .then(data => console.log(data));
+};
+
+const loadPost = () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+
+    fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            console.log(json);
+            displayPost(json);
+        })
+};
+
+const displayPost = (posts) => {
+    posts.forEach(post=>{
+        console.log(post)
+    })
 };
 
 
